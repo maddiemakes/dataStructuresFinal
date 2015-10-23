d8e274eaf94f26d8db8c1b1cfb42d685b9ec293f
Jordan <jwells.me@gmail.com>
2015-10-23 11:27:30 -0400

    comments

--- characters.h
+++ characters.h
@@ -1,7 +1,12 @@
 #include<stdio.h>
+
+//could make individual functions containing each individual character class?
+
 void player()
 {
 };
+
+//contains all enemies/allies etc.?
 void mobs()
 {
 };
