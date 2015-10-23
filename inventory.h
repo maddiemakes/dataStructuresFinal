8491eb0337a2f57485a754907bcd9af81f0dc9eb
jwellstech <jwells.me@gmail.com>
2015-10-23 11:22:56 -0400

    characters.h

--- inventory.h
+++ inventory.h
@@ -1,6 +1,13 @@
 #include<stdio.h>
 //stores player items
 //contains all items in the game
+
+//inventory() contains the user's personal inventory
 void inventory()
+{
+};
+
+//game items?
+void items()
 {
 };
