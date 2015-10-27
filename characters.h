296bb6d3d1c1f7c944ac19eb8d5f4c5541874bf2
MysteryPanda <yliu173546@gmail.com>
2015-10-27 08:50:14 -0400

    Update characters.h

--- characters.h
+++ characters.h
@@ -1,7 +1,14 @@
 #include<stdio.h>
 
 //could make individual functions containing each individual character class?
-
+struct Character{
+  char* name;
+  size_t STR,INT,WIS,END,LUK;
+  int class;
+}
+struct Class{
+  c
+}
 void player()
 {
 };
